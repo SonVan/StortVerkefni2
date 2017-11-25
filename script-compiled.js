66725f10a7ad078cf7aadb2f222977a4e4e9be83
ths220 <ths220@hi.is>
2017-11-25 11:48:52 +0000

    lint

--- script-compiled.js
+++ script-compiled.js
@@ -8,215 +8,214 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  * VideoPlayer
  */
 var Videoplayer = function () {
-		function Videoplayer() {
-				_classCallCheck(this, Videoplayer);
+  function Videoplayer() {
+    _classCallCheck(this, Videoplayer);
 
-				this.title_container = document.querySelector('#video__title');
-				var videoplayer = document.querySelector('.videoPlayer');
-				var back = document.querySelector('.button__back');
-				back.addEventListener('click', this.goBack.bind(this));
+    this.title_container = document.querySelector('.video__title');
+    var videoplayer = document.querySelector('.videoPlayer');
+    var back = document.querySelector('.button__back');
+    back.addEventListener('click', this.goBack.bind(this));
 
-				this.video_container = document.createElement('div');
-				this.video_container.setAttribute('class', 'video__container');
-				this.controls_container = document.createElement('div');
-				this.controls_container.setAttribute('class', 'video__controls');
+    this.video_container = document.createElement('div');
+    this.video_container.setAttribute('class', 'video__container');
+    this.controls_container = document.createElement('div');
+    this.controls_container.setAttribute('class', 'video__controls');
 
-				videoplayer.appendChild(this.video_container);
-				videoplayer.appendChild(this.controls_container);
-		}
+    videoplayer.appendChild(this.video_container);
+    videoplayer.appendChild(this.controls_container);
+  }
 
-		_createClass(Videoplayer, [{
-				key: 'goBack',
-				value: function goBack() {
-						window.location.href = "./";
-				}
-		}, {
-				key: 'load',
-				value: function load() {
-						this.createControlsElement();
-						this.createVideoElement();
-						this.id = window.location.search.substr(4);
-						this.fetchData();
-				}
-		}, {
-				key: 'fetchData',
-				value: function fetchData(data) {
-						var API_URL = "./videos.json";
-						var request = new XMLHttpRequest();
-						request.open('GET', API_URL, true);
-						var Videoplayer = this;
+  _createClass(Videoplayer, [{
+    key: 'goBack',
+    value: function goBack() {
+      window.location.href = './';
+    }
+  }, {
+    key: 'load',
+    value: function load() {
+      this.createControlsElement();
+      this.createVideoElement();
+      this.id = window.location.search.substr(4);
+      this.fetchData();
+    }
+  }, {
+    key: 'fetchData',
+    value: function fetchData() {
+      var _this = this;
 
-						request.onload = function () {
-								if (request.status < 400) {
-										var data = JSON.parse(request.response);
-										Videoplayer.loadVideo(data);
-								} else {
-										this.showError();
-								}
-						};
+      var API_URL = './videos.json';
+      var request = new XMLHttpRequest();
+      request.open('GET', API_URL, true);
+      var videoplayer = this;
 
-						request.onerror = function () {
-								this.showError();
-						};
+      request.onload = function () {
+        if (request.status < 400) {
+          var data = JSON.parse(request.response);
+          videoplayer.loadVideo(data);
+        } else {
+          _this.showError();
+        }
+      };
 
-						request.send();
-				}
-		}, {
-				key: 'play',
-				value: function play() {
-						this.buttons.play.setAttribute('class', 'button__hidden');
-						this.buttons.pause.setAttribute('class', 'button');
-						this.video_overlay.setAttribute('class', 'video__overlay__hidden');
-						this.video.play();
-				}
-		}, {
-				key: 'pause',
-				value: function pause() {
-						this.buttons.play.setAttribute('class', 'button');
-						this.buttons.pause.setAttribute('class', 'button__hidden');
-						this.video_overlay.setAttribute('class', 'video__overlay');
-						this.video.pause();
-				}
-		}, {
-				key: 'rewind',
-				value: function rewind() {
-						this.video.currentTime -= 3;
-				}
-		}, {
-				key: 'forward',
-				value: function forward() {
-						this.video.currentTime += 3;
-				}
-		}, {
-				key: 'mute',
-				value: function mute() {
-						this.buttons.mute.setAttribute('class', 'button__hidden');
-						this.buttons.unmute.setAttribute('class', 'button');
-						this.video.muted = true;
-				}
-		}, {
-				key: 'unmute',
-				value: function unmute() {
-						this.buttons.mute.setAttribute('class', 'button');
-						this.buttons.unmute.setAttribute('class', 'button__hidden');
-						this.video.muted = false;
-				}
-		}, {
-				key: 'fullscreen',
-				value: function fullscreen() {
-						this.launchIntoFullscreen(this.video);
-				}
-		}, {
-				key: 'launchIntoFullscreen',
-				value: function launchIntoFullscreen(element) {
-						if (element.requestFullscreen) {
-								element.requestFullscreen();
-						} else if (element.mozRequestFullScreen) {
-								element.mozRequestFullScreen();
-						} else if (element.webkitRequestFullscreen) {
-								element.webkitRequestFullscreen();
-						} else if (element.msRequestFullscreen) {
-								element.msRequestFullscreen();
-						}
-				}
-		}, {
-				key: 'showError',
-				value: function showError() {
-						var error = document.createElement('p');
-						error.appendChild(document.createTextNode('Videó er ekki til'));
-						this.video_container.insertBefore(error, this.video_container.firstChild);
-				}
-		}, {
-				key: 'loadVideo',
-				value: function loadVideo(data) {
-						if (!data.videos[this.id - 1]) {
-								this.showError();
-						} else {
-								this.video_overlay.setAttribute('class', 'video__overlay');
-								overlay.addEventListener('click', this.play.bind(this), false);
-								this.title_container.innerHTML = data.videos[this.id - 1].title;
-								this.video = this.video_container.querySelector('video');
-								this.videosrc = data.videos[this.id - 1].video;
-								this.video.setAttribute('src', this.videosrc);
-								this.videoposter = data.videos[this.id - 1].poster;
-								this.video.setAttribute('poster', this.videoposter);
-								this.video.addEventListener('ended', this.pause.bind(this), false);
-								this.video.addEventListener('click', this.pause.bind(this), false);
-								this.video.addEventListener('playing', this.play.bind(this), false);
-								this.video.addEventListener('pause', this.pause.bind(this), false);
-								this.loadControls();
-						}
-				}
-		}, {
-				key: 'loadControls',
-				value: function loadControls() {
-						this.buttons.play.addEventListener('click', this.play.bind(this));
-						this.buttons.pause.addEventListener('click', this.pause.bind(this));
-						this.buttons.rewind.addEventListener('click', this.rewind.bind(this));
-						this.buttons.forward.addEventListener('click', this.forward.bind(this));
-						this.buttons.mute.addEventListener('click', this.mute.bind(this));
-						this.buttons.unmute.addEventListener('click', this.unmute.bind(this));
-						this.buttons.fullscreen.addEventListener('click', this.fullscreen.bind(this));
-				}
-		}, {
-				key: 'createVideoElement',
-				value: function createVideoElement() {
-						this.video = document.createElement('video');
-						this.video.setAttribute('class', 'video');
-						this.video_overlay = document.createElement('div');
-						this.video_overlay.setAttribute('class', 'video__overlay__hidden');
-						this.video_overlay.setAttribute('id', 'overlay');
-						var playbutton = document.createElement('button');
-						playbutton.setAttribute('class', 'button');
-						playbutton.setAttribute('id', 'play');
+      request.onerror = function () {
+        _this.showError();
+      };
 
-						this.video_overlay.appendChild(playbutton);
-						this.video_container.appendChild(this.video);
-						this.video_container.appendChild(this.video_overlay);
-				}
-		}, {
-				key: 'createbutton',
-				value: function createbutton(name) {
-						var button = document.createElement('button');
-						button.setAttribute('class', 'button');
-						button.setAttribute('id', name);
-						this.controls_container.appendChild(button);
-						return button;
-				}
-		}, {
-				key: 'createControlsElement',
-				value: function createControlsElement() {
-						var rewind = this.createbutton('rewind');
-						var play = this.createbutton('play');
-						var pause = this.createbutton('pause');
-						pause.setAttribute('class', 'button__hidden');
-						var mute = this.createbutton('mute');
-						var unmute = this.createbutton('unmute');
-						unmute.setAttribute('class', 'button__hidden');
-						var fullscreen = this.createbutton('fullscreen');
-						var forward = this.createbutton('forward');
+      request.send();
+    }
+  }, {
+    key: 'play',
+    value: function play() {
+      this.buttons.play.setAttribute('class', 'button__hidden');
+      this.buttons.pause.setAttribute('class', 'button pause');
+      this.video_overlay.setAttribute('class', 'video__overlay__hidden');
+      this.video.play();
+    }
+  }, {
+    key: 'pause',
+    value: function pause() {
+      this.buttons.play.setAttribute('class', 'button play');
+      this.buttons.pause.setAttribute('class', 'button__hidden');
+      this.video_overlay.setAttribute('class', 'video__overlay');
+      this.video.pause();
+    }
+  }, {
+    key: 'rewind',
+    value: function rewind() {
+      this.video.currentTime -= 3;
+    }
+  }, {
+    key: 'forward',
+    value: function forward() {
+      this.video.currentTime += 3;
+    }
+  }, {
+    key: 'mute',
+    value: function mute() {
+      this.buttons.mute.setAttribute('class', 'button__hidden');
+      this.buttons.unmute.setAttribute('class', 'button unmute');
+      this.video.muted = true;
+    }
+  }, {
+    key: 'unmute',
+    value: function unmute() {
+      this.buttons.mute.setAttribute('class', 'button mute');
+      this.buttons.unmute.setAttribute('class', 'button__hidden');
+      this.video.muted = false;
+    }
+  }, {
+    key: 'fullscreen',
+    value: function fullscreen() {
+      this.launchIntoFullscreen(this.video);
+    }
+  }, {
+    key: 'launchIntoFullscreen',
+    value: function launchIntoFullscreen(element) {
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    }
+  }, {
+    key: 'showError',
+    value: function showError() {
+      var error = document.createElement('p');
+      error.appendChild(document.createTextNode('Videó er ekki til'));
+      this.video_container.insertBefore(error, this.video_container.firstChild);
+    }
+  }, {
+    key: 'loadVideo',
+    value: function loadVideo(data) {
+      if (!data.videos[this.id - 1]) {
+        this.showError();
+      } else {
+        this.video_overlay.setAttribute('class', 'video__overlay');
+        this.video_overlay.addEventListener('click', this.play.bind(this), false);
+        this.title_container.innerHTML = data.videos[this.id - 1].title;
+        this.video = this.video_container.querySelector('video');
+        this.videosrc = data.videos[this.id - 1].video;
+        this.video.setAttribute('src', this.videosrc);
+        this.videoposter = data.videos[this.id - 1].poster;
+        this.video.setAttribute('poster', this.videoposter);
+        this.video.addEventListener('ended', this.pause.bind(this), false);
+        this.video.addEventListener('click', this.pause.bind(this), false);
+        this.video.addEventListener('playing', this.play.bind(this), false);
+        this.video.addEventListener('pause', this.pause.bind(this), false);
+        this.loadControls();
+      }
+    }
+  }, {
+    key: 'loadControls',
+    value: function loadControls() {
+      this.buttons.play.addEventListener('click', this.play.bind(this));
+      this.buttons.pause.addEventListener('click', this.pause.bind(this));
+      this.buttons.rewind.addEventListener('click', this.rewind.bind(this));
+      this.buttons.forward.addEventListener('click', this.forward.bind(this));
+      this.buttons.mute.addEventListener('click', this.mute.bind(this));
+      this.buttons.unmute.addEventListener('click', this.unmute.bind(this));
+      this.buttons.fullscreen.addEventListener('click', this.fullscreen.bind(this));
+    }
+  }, {
+    key: 'createVideoElement',
+    value: function createVideoElement() {
+      this.video = document.createElement('video');
+      this.video.setAttribute('class', 'video');
+      this.video_overlay = document.createElement('div');
+      this.video_overlay.setAttribute('class', 'video__overlay__hidden');
+      var playbutton = document.createElement('button');
+      playbutton.setAttribute('class', 'button play');
 
-						this.buttons = {
-								play: play,
-								pause: pause,
-								rewind: rewind,
-								forward: forward,
-								mute: mute,
-								unmute: unmute,
-								fullscreen: fullscreen
-						};
-				}
-		}]);
+      this.video_overlay.appendChild(playbutton);
+      this.video_container.appendChild(this.video);
+      this.video_container.appendChild(this.video_overlay);
+    }
+  }, {
+    key: 'createbutton',
+    value: function createbutton(name) {
+      var button = document.createElement('button');
+      button.setAttribute('class', 'button ' + name);
+      this.controls_container.appendChild(button);
+      return button;
+    }
+  }, {
+    key: 'createControlsElement',
+    value: function createControlsElement() {
+      var rewind = this.createbutton('rewind');
+      var play = this.createbutton('play');
+      var pause = this.createbutton('pause');
+      pause.setAttribute('class', 'button__hidden');
+      var mute = this.createbutton('mute');
+      var unmute = this.createbutton('unmute');
+      unmute.setAttribute('class', 'button__hidden');
+      var fullscreen = this.createbutton('fullscreen');
+      var forward = this.createbutton('forward');
 
-		return Videoplayer;
+      this.buttons = {
+        play: play,
+        pause: pause,
+        rewind: rewind,
+        forward: forward,
+        mute: mute,
+        unmute: unmute,
+        fullscreen: fullscreen
+      };
+    }
+  }]);
+
+  return Videoplayer;
 }();
 
-document.addEventListener("DOMContentLoaded", function () {
-		var currentUrlLastEntry = window.location.pathname.split("/").pop();
-		if (currentUrlLastEntry === "video.html") {
-				var videoplayer = new Videoplayer();
-				videoplayer.load();
-		}
+document.addEventListener('DOMContentLoaded', function () {
+  var currentUrlLastEntry = window.location.pathname.split('/').pop();
+  if (currentUrlLastEntry === 'video.html') {
+    var videoplayer = new Videoplayer();
+    videoplayer.load();
+  }
 });
 'use strict';
 
--- script-video.js
+++ script-video.js
@@ -2,193 +2,186 @@
  * VideoPlayer
  */
 class Videoplayer {
-
   constructor() {
-	this.title_container = document.querySelector('#video__title');
-	var videoplayer = document.querySelector('.videoPlayer');
-	var back = document.querySelector('.button__back');
-	back.addEventListener('click', this.goBack.bind(this));
+    this.title_container = document.querySelector('.video__title');
+    const videoplayer = document.querySelector('.videoPlayer');
+    const back = document.querySelector('.button__back');
+    back.addEventListener('click', this.goBack.bind(this));
 
-  this.video_container = document.createElement('div');
-	this.video_container.setAttribute('class', 'video__container');
-  this.controls_container = document.createElement('div');
-	this.controls_container.setAttribute('class', 'video__controls');
+    this.video_container = document.createElement('div');
+    this.video_container.setAttribute('class', 'video__container');
+    this.controls_container = document.createElement('div');
+    this.controls_container.setAttribute('class', 'video__controls');
 
-	videoplayer.appendChild(this.video_container);
-	videoplayer.appendChild(this.controls_container);
+    videoplayer.appendChild(this.video_container);
+    videoplayer.appendChild(this.controls_container);
   }
 
   goBack() {
-	window.location.href = "./";
+    window.location.href = './';
   }
 
   load() {
-	this.createControlsElement();
-	this.createVideoElement();
-	this.id = window.location.search.substr(4);
-	this.fetchData();
+    this.createControlsElement();
+    this.createVideoElement();
+    this.id = window.location.search.substr(4);
+    this.fetchData();
   }
 
-  fetchData(data){
-  	var API_URL = "./videos.json";
-    var request = new XMLHttpRequest();
+  fetchData() {
+    const API_URL = './videos.json';
+    const request = new XMLHttpRequest();
     request.open('GET', API_URL, true);
-    var Videoplayer = this;
+    const videoplayer = this;
 
-	request.onload = function() {
+    request.onload = () => {
       if (request.status < 400) {
-		var data = JSON.parse(request.response);
-		Videoplayer.loadVideo(data);
+        const data = JSON.parse(request.response);
+        videoplayer.loadVideo(data);
       } else {
         this.showError();
       }
     };
 
-    request.onerror = function() {
+    request.onerror = () => {
       this.showError();
     };
 
     request.send();
-
   }
 
   play() {
-	this.buttons.play.setAttribute('class', 'button__hidden');
-	this.buttons.pause.setAttribute('class', 'button');
-	this.video_overlay.setAttribute('class', 'video__overlay__hidden');
-	this.video.play();
+    this.buttons.play.setAttribute('class', 'button__hidden');
+    this.buttons.pause.setAttribute('class', 'button pause');
+    this.video_overlay.setAttribute('class', 'video__overlay__hidden');
+    this.video.play();
   }
 
   pause() {
-	this.buttons.play.setAttribute('class', 'button');
-	this.buttons.pause.setAttribute('class', 'button__hidden');
-	this.video_overlay.setAttribute('class', 'video__overlay');
-	this.video.pause();
+    this.buttons.play.setAttribute('class', 'button play');
+    this.buttons.pause.setAttribute('class', 'button__hidden');
+    this.video_overlay.setAttribute('class', 'video__overlay');
+    this.video.pause();
   }
 
   rewind() {
-	this.video.currentTime -= 3;
+    this.video.currentTime -= 3;
   }
 
   forward() {
-	this.video.currentTime += 3;
+    this.video.currentTime += 3;
   }
 
   mute() {
-	this.buttons.mute.setAttribute('class', 'button__hidden');
-	this.buttons.unmute.setAttribute('class', 'button');
-	this.video.muted = true;
+    this.buttons.mute.setAttribute('class', 'button__hidden');
+    this.buttons.unmute.setAttribute('class', 'button unmute');
+    this.video.muted = true;
   }
 
   unmute() {
-	this.buttons.mute.setAttribute('class', 'button');
-	this.buttons.unmute.setAttribute('class', 'button__hidden');
-	this.video.muted = false;
+    this.buttons.mute.setAttribute('class', 'button mute');
+    this.buttons.unmute.setAttribute('class', 'button__hidden');
+    this.video.muted = false;
   }
 
   fullscreen() {
     this.launchIntoFullscreen(this.video);
-
   }
 
   launchIntoFullscreen(element) {
-    if(element.requestFullscreen) {
+    if (element.requestFullscreen) {
       element.requestFullscreen();
-    } else if(element.mozRequestFullScreen) {
+    } else if (element.mozRequestFullScreen) {
       element.mozRequestFullScreen();
-    } else if(element.webkitRequestFullscreen) {
+    } else if (element.webkitRequestFullscreen) {
       element.webkitRequestFullscreen();
-    } else if(element.msRequestFullscreen) {
+    } else if (element.msRequestFullscreen) {
       element.msRequestFullscreen();
     }
   }
 
   showError() {
-	var error = document.createElement('p');
-	error.appendChild(document.createTextNode('Videó er ekki til'));
-	this.video_container.insertBefore(error, this.video_container.firstChild);
+    const error = document.createElement('p');
+    error.appendChild(document.createTextNode('Videó er ekki til'));
+    this.video_container.insertBefore(error, this.video_container.firstChild);
   }
 
   loadVideo(data) {
-	if(!data.videos[this.id-1]){
-	  this.showError();
-	}
-	else {
-	  this.video_overlay.setAttribute('class', 'video__overlay');
-	  overlay.addEventListener('click',this.play.bind(this),false);
-	  this.title_container.innerHTML = data.videos[this.id-1].title;
-	  this.video = this.video_container.querySelector('video');
-	  this.videosrc = data.videos[this.id-1].video;
-	  this.video.setAttribute('src', this.videosrc);
-	  this.videoposter = data.videos[this.id-1].poster;
-	  this.video.setAttribute('poster', this.videoposter);
-	  this.video.addEventListener('ended',this.pause.bind(this),false);
-	  this.video.addEventListener('click',this.pause.bind(this),false);
-	  this.video.addEventListener('playing', this.play.bind(this),false);
-	  this.video.addEventListener('pause', this.pause.bind(this),false);
-	  this.loadControls();
-	}
+    if (!data.videos[this.id - 1]) {
+      this.showError();
+    } else {
+      this.video_overlay.setAttribute('class', 'video__overlay');
+      this.video_overlay.addEventListener('click', this.play.bind(this), false);
+      this.title_container.innerHTML = data.videos[this.id - 1].title;
+      this.video = this.video_container.querySelector('video');
+      this.videosrc = data.videos[this.id - 1].video;
+      this.video.setAttribute('src', this.videosrc);
+      this.videoposter = data.videos[this.id - 1].poster;
+      this.video.setAttribute('poster', this.videoposter);
+      this.video.addEventListener('ended', this.pause.bind(this), false);
+      this.video.addEventListener('click', this.pause.bind(this), false);
+      this.video.addEventListener('playing', this.play.bind(this), false);
+      this.video.addEventListener('pause', this.pause.bind(this), false);
+      this.loadControls();
+    }
   }
 
   loadControls() {
-	this.buttons.play.addEventListener('click', this.play.bind(this));
-	this.buttons.pause.addEventListener('click', this.pause.bind(this));
-	this.buttons.rewind.addEventListener('click', this.rewind.bind(this));
-	this.buttons.forward.addEventListener('click', this.forward.bind(this));
-	this.buttons.mute.addEventListener('click', this.mute.bind(this));
-	this.buttons.unmute.addEventListener('click', this.unmute.bind(this));
-	this.buttons.fullscreen.addEventListener('click', this.fullscreen.bind(this));
+    this.buttons.play.addEventListener('click', this.play.bind(this));
+    this.buttons.pause.addEventListener('click', this.pause.bind(this));
+    this.buttons.rewind.addEventListener('click', this.rewind.bind(this));
+    this.buttons.forward.addEventListener('click', this.forward.bind(this));
+    this.buttons.mute.addEventListener('click', this.mute.bind(this));
+    this.buttons.unmute.addEventListener('click', this.unmute.bind(this));
+    this.buttons.fullscreen.addEventListener('click', this.fullscreen.bind(this));
   }
 
   createVideoElement() {
-	this.video = document.createElement('video');
-	this.video.setAttribute('class', 'video');
-	this.video_overlay = document.createElement('div');
-	this.video_overlay.setAttribute('class', 'video__overlay__hidden');
-	this.video_overlay.setAttribute('id', 'overlay');
-	var playbutton = document.createElement('button');
-	playbutton.setAttribute('class', 'button');
-	playbutton.setAttribute('id', 'play');
+    this.video = document.createElement('video');
+    this.video.setAttribute('class', 'video');
+    this.video_overlay = document.createElement('div');
+    this.video_overlay.setAttribute('class', 'video__overlay__hidden');
+    const playbutton = document.createElement('button');
+    playbutton.setAttribute('class', 'button play');
 
-	this.video_overlay.appendChild(playbutton);
-	this.video_container.appendChild(this.video);
-	this.video_container.appendChild(this.video_overlay);
+    this.video_overlay.appendChild(playbutton);
+    this.video_container.appendChild(this.video);
+    this.video_container.appendChild(this.video_overlay);
   }
 
   createbutton(name) {
-	var button = document.createElement('button');
-	button.setAttribute('class', 'button');
-	button.setAttribute('id', name);
-	this.controls_container.appendChild(button);
-	return button;
+    const button = document.createElement('button');
+    button.setAttribute('class', `button ${name}`);
+    this.controls_container.appendChild(button);
+    return button;
   }
 
   createControlsElement() {
-	var rewind = this.createbutton('rewind');
-	var play = this.createbutton('play');
-	var pause = this.createbutton('pause');
-	pause.setAttribute('class', 'button__hidden');
-	var mute = this.createbutton('mute');
-	var unmute = this.createbutton('unmute');
-	unmute.setAttribute('class', 'button__hidden');
-	var fullscreen = this.createbutton('fullscreen');
-	var forward = this.createbutton('forward');
+    const rewind = this.createbutton('rewind');
+    const play = this.createbutton('play');
+    const pause = this.createbutton('pause');
+    pause.setAttribute('class', 'button__hidden');
+    const mute = this.createbutton('mute');
+    const unmute = this.createbutton('unmute');
+    unmute.setAttribute('class', 'button__hidden');
+    const fullscreen = this.createbutton('fullscreen');
+    const forward = this.createbutton('forward');
 
-	this.buttons = {
-	  play,
-	  pause,
-	  rewind,
-	  forward,
-	  mute,
-	  unmute,
-	  fullscreen,
-	}
+    this.buttons = {
+      play,
+      pause,
+      rewind,
+      forward,
+      mute,
+      unmute,
+      fullscreen,
+    };
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var currentUrlLastEntry = window.location.pathname.split("/").pop();
-  if (currentUrlLastEntry === "video.html") {
+document.addEventListener('DOMContentLoaded', () => {
+  const currentUrlLastEntry = window.location.pathname.split('/').pop();
+  if (currentUrlLastEntry === 'video.html') {
     const videoplayer = new Videoplayer();
     videoplayer.load();
   }
